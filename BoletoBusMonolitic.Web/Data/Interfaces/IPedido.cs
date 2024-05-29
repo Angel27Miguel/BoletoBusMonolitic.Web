f745a1a19ee8f27a4daa273317f4266bc5a6bf88
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-05-28 23:54:53 -0400

    Ismael Actualización
    Todo lo de Ismael esta unido a la rama master

---
+++ BoletoBusMonolitic.Web/Data/Interfaces/IPedido.cs
@@ -0,0 +1,12 @@
+﻿using BoletoBusMonolitic.Web.Data.Entites;
+
+namespace BoletoBusMonolitic.Web.Data.Interfaces
+{
+    public interface IPedido
+    {
+        List<Pedido> GetPedidoList();
+        public void Actualizar();
+        public void Eliminar();
+        public void Agregar();   
+    }
+}
