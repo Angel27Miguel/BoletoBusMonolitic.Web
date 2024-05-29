bed51550e77ded8bea1548bb4c62e0debb9349fd
DanielAlexander2004 <Danielalexand09@outlook.es>
2024-05-28 23:32:47 -0400

    Add files via upload

---
+++ BoletoBusMonolitic.Web/Data/Interfaces/ICliente.cs
@@ -0,0 +1,17 @@
+﻿using BoletoBusMonolitic.Web.Data.Entites;
+
+namespace BoletoBusMonolitic.Web.Data.Interfaces
+{
+    public interface ICliente
+    {
+        List<Cliente> GetClienteList();
+
+        public void AgregarCliente();
+
+        public void EliminarCliente();
+
+        public void MostrarClientes();
+    }
+}
+
+/*Daniel Alexander Dominguez*/
---
+++ BoletoBusMonolitic.Web/Data/Interfaces/Ifactura.cs
@@ -0,0 +1,17 @@
+﻿using BoletoBusMonolitic.Web.Data.Entites;
+
+namespace BoletoBusMonolitic.Web.Data.Interfaces
+{
+    public interface IFactura
+    {
+        List<Factura> GetFacturaList();
+
+        public void AgregarFactura();
+
+        public void EliminarFactura();
+
+        public void MostrarFactura();
+    }
+}
+
+/*Daniel Alexander Dominguez*/
