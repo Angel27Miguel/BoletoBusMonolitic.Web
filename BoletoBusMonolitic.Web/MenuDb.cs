436bb84653ed95f14fa818ef1bf130a2f5e1dad7
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-05-28 23:45:04 -0400

    Add files via upload

---
+++ BoletoBusMonolitic.Web/MenuDb.cs
@@ -0,0 +1,33 @@
+﻿using BoletoBusMonolitic.Web.Data.Entites;
+using BoletoBusMonolitic.Web.Data.Interfaces;
+
+namespace BoletoBusMonolitic.Web.Data.Daos
+{
+    public class MenuDb : IMenu
+    {
+        public void Actualizar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Agregar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Eliminar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Menu> GetMenuList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Mostrar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
