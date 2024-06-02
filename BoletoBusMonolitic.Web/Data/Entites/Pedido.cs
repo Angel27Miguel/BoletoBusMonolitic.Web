5b02cd96a072b738af6bbb25ee246cfcb2ef6ddb
Ismael Garcia <ismaelgar336@gmail.com>
2024-06-01 22:37:25 -0400

    Actualizacion V1

--- BoletoBusMonolitic.Web/Data/Entites/Pedido.cs
+++ BoletoBusMonolitic.Web/Data/Entites/Pedido.cs
@@ -4,15 +4,19 @@ namespace BoletoBusMonolitic.Web.Data.Entites
 {
     public class Pedido : BaseEntity
     {
+        //atributos
+        private int IdPedido;
         private int IdCliente;
         private int IdMesa;
         private DateTime Fecha;
         private double Total;
-        public Pedido(int id, int IdCliente, int IdMesa, DateTime Fecha, double Total) : base(id)
+
+        public Pedido(int Id, int IdPedido, int IdCliente, int IdMesa, DateTime Fecha, double Total) : base(Id)
         {
+            this.IdPedido = IdPedido;
             this.IdCliente = IdCliente;
             this.IdMesa = IdMesa;   
-            this.Fecha = Fecha;
+            this.Fecha = Fecha;     
             this.Total = Total;
         }
     }
