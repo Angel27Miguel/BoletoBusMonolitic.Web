2c30e5644f0a022aa239f1d77533f8c662f3f77b
Ismael Garcia <ismaelgar336@gmail.com>
2024-06-09 01:25:05 -0400

    Actulizacion V5

--- BoletoBusMonolitic.Web/Controllers/HomeController.cs
+++ BoletoBusMonolitic.Web/Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BoletoBusMonolitic.Web.Controllers
+namespace SchoolMonoliticWeb.Controllers
 {
     public class HomeController : Controller
     {
