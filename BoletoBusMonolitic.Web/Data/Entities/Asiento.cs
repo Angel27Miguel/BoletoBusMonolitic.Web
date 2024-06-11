831e59d95419ad0856b77bcd60bad487d2f14620
AngelNaut <118201609+AngelNaut@users.noreply.github.com>
2024-06-11 18:24:00 -0400

    Add files via upload

---
+++ BoletoBusMonolitic.Web/Data/Entities/Asiento.cs
@@ -0,0 +1,15 @@
+﻿using BoletoBusMonolitic.Web.Data.Core;
+
+namespace BoletoBusMonolitic.Web.Data.Entites
+{
+    public class Asiento : BaseEntity
+    {
+        private int idBus;
+        private int numeroPiso;
+        private int numeroAsiento;
+        private DateTime fechaCreacion;
+        public Asiento(int id) : base(id)
+        {
+        }
+    }
+}
---
+++ BoletoBusMonolitic.Web/Data/Entities/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using BoletoBusMonolitic.Web.Data.Core;
+
+namespace BoletoBusMonolitic.Web.Data.Entites
+{
+    public class Usuario : BaseEntity
+    {
+        // atributos de la entidad 
+        
+        private string nombre;
+        private string apellidos;
+        private string correo;
+        private string clave;
+        private string TipoUsuario;
+        private DateTime fechaCreacion;
+
+        public Usuario(int id) : base(id)
+        {
+
+        }
+    }
+}
