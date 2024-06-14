ae513c0f38dbbddd21937a46a26f4a701476dc56
DanielAlexander2004 <Danielalexand09@outlook.es>
2024-06-14 11:47:48 -0400

    Add files via upload

--- BoletoBusMonolitic.Web/Data/Context/BoletoBusContext.cs
+++ BoletoBusMonolitic.Web/Data/Context/BoletoBusContext.cs
@@ -1,7 +1,23 @@
-﻿namespace BoletoBusMonolitic.Web.Data.Context
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BoletoBusMonolitic.Web.Data.Context
 {
-    public class BoletoBusContext
+    public class BoletoBusContext: DbContext
     {
+        public BoletoBusContext(DbContextOptions<BoletoBusContext> options) : base(options) { }
+
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Factura>  Factura { get; set; }
+        public DbSet<Mesa> Mesa { get; set; }
+        public DbSet<BoletoBusMonolitic.Web.Data.Models.ClienteModel> clienteModels { get; set; } = default!;
+        public DbSet<BoletoBusMonolitic.Web.Data.Models.FacturaModel> facturaModels { get; set; } = default!;
+        public DbSet<BoletoBusMonolitic.Web.Data.Models.MesaModel> mesaModels { get; set; } = default!;
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+
 
     }
 }
