388b7e177d318470efa56f38c602805a24b1d8f1
DanielAlexander2004 <Danielalexand09@outlook.es>
2024-06-14 11:51:04 -0400

    Add files via upload

--- BoletoBusMonolitic.Web/Data/Entites/Cliente.cs
+++ BoletoBusMonolitic.Web/Data/Entites/Cliente.cs
@@ -3,12 +3,16 @@ using BoletoBusMonolitic.Web.Data.Core;
 
 namespace BoletoBusMonolitic.Web.Data{
     public class Cliente:BaseEntity {
+        public int IdCliente;
+        public string Nombre;
+        public int Telefono;
+        public  string?Correo;
 
-        private string? Nombre;
-        private int Telefono;
-        private string? Correo;
+        public Cliente()
+        {
+        }
 
-        public Cliente(int id, string? Nombre, int Telefono, string? Correo ) : base(id)
+        public Cliente(int id, string? Nombre, int Telefono, string? Correo ) : base()
         {
         this.Nombre = Nombre;
         this.Telefono = Telefono;
--- BoletoBusMonolitic.Web/Data/Entites/Factura.cs
+++ BoletoBusMonolitic.Web/Data/Entites/Factura.cs
@@ -5,16 +5,24 @@ namespace BoletoBusMonolitic.Web.Data
 {
     public class Factura:BaseEntity
     {
-        private int Numerodefactura;
-        private string? Cliente;
-        private DateTime Fechadevencimiento;
+        public int IDFactura;
+        public int IDPedido;
+        public int Total;
+        public DateTime Fecha;
 
-        public Factura(int id, int Numerodefactura, string? Cliente, DateTime Fechadevencimiento) : base(id)
+        public Factura() { }
+
+        public Factura(int id, int IDFactura, int IDPedido, int Total, DateTime Fecha) : base()
         {
-        this.Numerodefactura = Numerodefactura;
-        this.Cliente = Cliente;
-        this.Fechadevencimiento = Fechadevencimiento;
+            Id = id;
+            this.IDFactura = IDFactura;
+            this.IDPedido = IDPedido;
+            this.Total = Total;
+            this.Fecha = Fecha;
         }
+
+        public int Id { get; }
+        public int IdFactura { get; internal set; }
     }
 
 }
