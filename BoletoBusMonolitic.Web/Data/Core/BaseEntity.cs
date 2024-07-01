6233e817f3b90426d7aa47afb0f7897d2f0507e5
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-07-01 19:35:35 -0400

    Ajuste

--- BoletoBusMonolitic.Web/Data/Core/BaseEntity.cs
+++ BoletoBusMonolitic.Web/Data/Core/BaseEntity.cs
@@ -2,6 +2,10 @@ namespace BoletoBusMonolitic.Web.Data.Core
 {
     public abstract class BaseEntity
     {
+        protected BaseEntity() { 
+        this.FechaCreacion = DateTime.Now;
+
+        }
         public DateTime FechaCreacion { get; set; }
 
     }
